db9d159f96af5d0d5495f3fc9c84b563962f1c77
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2017-10-18 10:39:17 +1100

    Initial commit

---
+++ ci/build.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+set -e
+set -x
+set -o pipefail
+set -u
+
+ORIG_PWD="${PWD}"
+
+# Create our own GOPATH
+export GOPATH="${ORIG_PWD}/go"
+
+# Symlink our source dir from inside of our own GOPATH
+mkdir -p "${GOPATH}/src/github.com/govau"
+ln -s "${ORIG_PWD}/src" "${GOPATH}/src/github.com/govau/cf-healthcheck"
+cd "${GOPATH}/src/github.com/govau/cf-healthcheck"
+
+# Cache glide deps
+export GLIDE_HOME="${ORIG_PWD}/src/.glide_cache"
+mkdir -p "${GLIDE_HOME}"
+
+# Install go deps
+glide install
+
+# Write our status file
+DATE="$(date)"
+ID="$(openssl rand -hex 32)"
+mkdir -p "${ORIG_PWD}/src/data"
+cat <<EOF > "${ORIG_PWD}/src/data/status.json"
+{
+    "id": "${ID}",
+    "date": "${DATE}"
+}
+EOF
+
+# Generate the stuff
+go generate
+
+# Build the thing
+go build
+
+# Run Go tests
+go test $(go list ./... | grep -v "/vendor/")
+
+# Copy artefacts to output directory
+cp -R \
+        "${ORIG_PWD}/src/cf-healthcheck" \
+        "${ORIG_PWD}/src/Procfile" \
+    "${ORIG_PWD}/build"
+
+# Write manifest
+cp "${ORIG_PWD}/src/manifest-template.yml" "${ORIG_PWD}/manifest/manifest.yml"
+printf "\ndomain: $DOMAIN\n" >> "${ORIG_PWD}/manifest/manifest.yml"
+
+# Write SHA256
+EXPECTED_RESULT="$(openssl dgst -sha256 < "${ORIG_PWD}/src/data/status.json")"
+cat <<EOF > "${ORIG_PWD}/test/check.sh"
+#!/bin/bash
+
+set -e
+set -x
+set -o pipefail
+set -u
+
+RESULT="\$(curl "http://cf-healthcheck.${DOMAIN}" | openssl dgst -sha256)"
+
+[[ "\${RESULT}" -eq "${EXPECTED_RESULT}" ]] || exit 1
+EOF
+
+chmod a+x "${ORIG_PWD}/test/check.sh"
+
+echo "Script to test is:"
+cat ${ORIG_PWD}/test/check.sh"
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+//go:generate go-bindata -o static.go data/
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	data, err := Asset("data/status.json")
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write(data)
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil))
+}
